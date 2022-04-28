0323dc311810d367d373f230a114d8ff91a6a2b3
Elad <eladbendavid7@gmail.com>
2022-04-28 22:24:27 +0300

    updated componenets

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-class App extends React.Component {
-    render() {
-        return <div>deployment</div>;
-    }
-}
+
 ReactDOM.render(<App />, document.querySelector("#root"));
