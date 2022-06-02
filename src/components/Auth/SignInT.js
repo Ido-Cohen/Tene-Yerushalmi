dd32a08d0f8c7227045943ec0b77a0b75246041c
Your Name <bennahum6@gmail.com>
2022-06-02 13:46:25 +0300

    update new SignIn design finish.

--- src/components/Auth/SignInT.js
+++ src/components/Auth/SignInT.js
@@ -13,14 +13,15 @@ export default function SignInT() {
         ```
       */}
             <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                <div className="max-w-md w-full space-y-8 white pt-5">
+                <div className="max-w-md w-full space-y-8 white pt-5 rounded-lg">
                     <div>
                         <img
                             className="mx-auto h-12 w-auto"
-                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                            src={"/../../../img/tenelogo.png"}
                             alt="Workflow"
+
                         />
-                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">התחברות לאתר הבוגרים</h2>
                     </div>
                     <form className="mt-8 space-y-6" action="#" method="POST">
                         <input type="hidden" name="remember" defaultValue="true" />
@@ -55,22 +56,10 @@ export default function SignInT() {
                             </div>
                         </div>
 
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center">
-                                <input
-                                    id="remember-me"
-                                    name="remember-me"
-                                    type="checkbox"
-                                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                                />
-                                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                                    Remember me
-                                </label>
-                            </div>
-
+                        <div className="flex items-center justify-end">
                             <div className="text-sm">
-                                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                    Forgot your password?
+                                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500 text-right">
+                                   ?שכחת סיסמה
                                 </a>
                             </div>
                         </div>
@@ -78,12 +67,12 @@ export default function SignInT() {
                         <div>
                             <button
                                 type="submit"
-                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-orange-400 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                  <LockClosedIcon className="h-5 w-5 text-white group-hover:text-white" aria-hidden="true" />
                 </span>
-                                Sign in
+                                התחברות
                             </button>
                         </div>
                     </form>
