e4b53567f8d982d310800340a0236fdd4bce2fa8
Your Name <bennahum6@gmail.com>
2022-06-02 19:47:05 +0300

    update

---
+++ src/components/Auth/Dropdown.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
+
+const Dropdown = (props) => {
+    console.log(props)
+    const {type,values,reference} = props;
+    const [value, setValue] = useState('');
+    const handleChange = (event) => {
+        setValue(event.target.value);
+        if (reference)
+            reference(event.target.value);
+    };
+    return (
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">{type}</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={value}
+                    label={type}
+                    onChange={handleChange}
+                >
+                    {values.map((num) => {
+                        return (
+                            <MenuItem value={num.value} style={{justifyContent: "right"}} key={num.value}>{num.label}</MenuItem>)
+                    })}
+                </Select>
+            </FormControl>
+        </Box>
+    );
+};
+
+export default Dropdown;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
@@ -7,6 +7,6 @@ module.exports = {
     require("@tailwindcss/forms")({
       strategy: 'base', // only generate global styles
       // strategy: 'class', // only generate classes
-    }),
+    }),require('flowbite/plugin')
   ],
 }
