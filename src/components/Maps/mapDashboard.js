93f3d67efafdcf6ff3e420fc21a2b2e8dde7358b
Elad <eladbendavid7@gmail.com>
2022-06-07 02:31:56 +0300

    added map working with google maps

--- src/components/Maps/mapDashboard.js
+++ src/components/Maps/mapDashboard.js
@@ -1,30 +1,64 @@
-import React, {useState} from 'react';
-import {Map, Marker} from "pigeon-maps";
-import moment from "moment";
-
+import React, {useMemo, useState} from 'react';
+import {GoogleMap,useLoadScript,Marker} from '@react-google-maps/api'
+import usePlacesAutocomplete, {getGeocode, getLatLng} from "use-places-autocomplete";
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+    ComboboxOptionText,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 const MapDashboard = () => {
-    const [state, setState] = useState({lat: '', lin: ''});
-    const anchorHover = (e) => {
-        setState({lat: e.anchor[0], lin: e.anchor[1]});
-    }
-    return (
-        <div className={"container center"}>
-            <Map height={300} defaultCenter={[31.771959, 35.217018]} defaultZoom={7}>
-                <Marker width={50} anchor={[31.771959, 35.217018]} onClick={(e) => anchorHover(e)}/>
-            </Map>
-            {state.lat ?
-                <div className={"card z-depth-0"}>
-                    <div className={"card-content"}>
-                        <span className="card-title">maps trying</span>
-                        <p>trying here</p>
-                    </div>
-                    <div className={"card-action gret lighten-4 grey-text"}>
-                        <div>heyyy im here</div>
-                    </div>
-                </div>
-                : null}
-        </div>
-    );
-};
+    const {isLoaded} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_API_KEY,
+        libraries : ['places']
 
+    })
+    const [state, setState] = useState({lat: '', lin: ''});
+    if (!isLoaded) return <div>Loading...</div>;
+    return <Map/>
+};
+function Map() {
+    const [selected, setSelected] = useState(null);
+
+    const center = useMemo(() => ({lat: 31.4117257, lng: 35.0818155}),[]);
+    return (<>
+        <div className={"places-container"}>
+            <PlacesAutoComplete setSelected={setSelected}/>
+        </div>
+        <GoogleMap zoom={10} center={center} mapContainerStyle={{width: '100%', height: '100vh'}}>
+            {selected && <Marker position={selected}/>}
+    </GoogleMap>
+        </>
+
+    );
+}
+
+const PlacesAutoComplete = ({setSelected}) => {
+    const {
+        ready,
+        value,
+        setValue,
+        suggestions: {status,data},
+        clearSuggestions
+    } = usePlacesAutocomplete()
+
+    const handleSelect = async (address) => {
+      setValue(address,false);
+      clearSuggestions();
+      const result = await getGeocode({address});
+      const {lat,lng} = await getLatLng(result[0]);
+      setSelected({lat,lng});
+    }
+    return <Combobox onSelect={handleSelect}>
+        <ComboboxInput value={value} onChange={(e) => {setValue(e.target.value)}} disabled={!ready} className={'combobox-input'} placeholder={"Search an address"}/>
+        <ComboboxPopover>
+            <ComboboxList>
+                {status === 'OK' && data.map(({place_id,description}) => (<ComboboxOption key={place_id} value={description}/>))}
+            </ComboboxList>
+        </ComboboxPopover>
+    </Combobox>
+}
 export default MapDashboard;
