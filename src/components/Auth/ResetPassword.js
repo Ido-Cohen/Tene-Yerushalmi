b86afbeabd9a64fb570f4f6f5d769f39950807bd
Elad <eladbendavid7@gmail.com>
2022-06-10 15:56:08 +0300

    updated map and user landing page

--- src/components/Auth/ResetPassword.js
+++ src/components/Auth/ResetPassword.js
@@ -3,18 +3,27 @@ import {connect} from "react-redux";
 import {createMessage} from "../../store/actions/messageActions";
 import {Navigate} from "react-router";
 import {passwordReset} from "../../store/actions/authActions";
+import axios from "axios";
 
 const CreateMessage = (props) => {
     const {auth, passwordReset} = props;
-    const [state, setState] = useState('');
+    const [email, setEmail] = useState("");
+    const [response,setResponse] = useState(null);
 
     const handleChange = (e) => {
-        setState(e.target.value);
+        setEmail(e.target.value);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        passwordReset(state);
+        axios.post('/resetpassword',{email: email}).then(res =>{
+            console.log(res);
+            setResponse(res.data);
+        }).catch(err => {
+            console.log(err);
+            setResponse(err);
+        })
+        // passwordReset(state);
         // window.open('/signin');
     }
 
@@ -37,19 +46,21 @@ const CreateMessage = (props) => {
 
         <div className="blue-grey-text min-h-full flex flex-col py-12 px-4 sm:px-6 lg:px-8">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-1 ">
-                <div className="bg-blue px-1 py-8 rounded shadow-md text-black w-full rounded-lg">
+                <div className="bg-blue px-1 py-8 rounded shadow-md text-black w-full rounded-lg bg-white/90">
                     <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
                         <h2 className="mb-8 text-3xl text-center">איפוס סיסמה</h2>
                         <input type="text" id={"email"}
                                className="block border border-grey-light w-full p-3 rounded mb-4 text-right"
                                name="email"
                                placeholder="אימייל"
+                               onChange={handleChange}
                         />
                         <button type="submit" id={"submit"}
                                 className="w-full text-center py-3 rounded text-white bg-orange-400 hover:bg-orange-600 focus:outline-none my-1 text-center">
                             איפוס חשבון
                         </button>
                     </form>
+                    {response ? (response.err ? <p className={"text-red-600 text-center"}>משהו השתבש</p> : <p className={"text-blue-600 text-center"}>{response.success}</p>) : ''}
                 </div>
             </div>
         </div>
@@ -57,6 +68,7 @@ const CreateMessage = (props) => {
 
 }
 const mapStateToProps = (state) => {
+    console.log(state)
     return {
         auth: state.firebase.auth
     }
--- src/components/Dashboard/DashboardT.js
+++ src/components/Dashboard/DashboardT.js
@@ -6,35 +6,56 @@ import {compose} from "redux";
 import {Navigate} from "react-router";
 import MessageListT from "../Messages/MessageListT";
 
-class DashboardT extends Component {
-    render() {
-        const {messages, auth} = this.props;
-        if (!auth.uid) {
-            return <Navigate replace to={'/signin'}/>
-        }
-        return (
-            <div className={"dashboard container justify-center"}>
+const DashboardT = (props) => {
 
-                {/*nav bar*/}
-
-
-
-                <div className={"row"}>
-                    <MessageListT messages={messages}/>
-                </div>
-            </div>
-        )
+    const {messages, auth, currentUser, isAdmin} = props;
+    if (!auth.uid) {
+        return <Navigate replace to={'/signin'}/>
     }
+    if (currentUser && currentUser[0].isNewUser) {
+        return <Navigate replace to={'/reset-password/new-user'}/>
+    }
+    console.log(messages)
+    let sorted;
+    if (currentUser && messages) {
+        sorted = messages.slice().sort((a, b) => b.createdAt.toDate() - a.createdAt.toDate());
+        console.log(sorted)
+        if (!isAdmin) {
+            sorted = sorted.filter(msg => {
+                return msg?.yearOfGraduate === currentUser[0]?.yearOfGraduate;
+            })
+        }
+    }
+    return (
+        <div className={"dashboard container justify-center"}>
+            <div className={"row"}>
+                <MessageListT messages={sorted}/>
+            </div>
+        </div>
+    )
 }
 
+
 const mapStateToProps = (state) => {
+    const handle = state.auth.handle;
+    const users = state.firestore.ordered.users;
+    let user;
+    if (users) {
+        user = users.filter(user => {
+            return user.handle === handle;
+        });
+    }
     return {
         messages: state.firestore.ordered.messages,
         auth: state.firebase.auth,
+        isAdmin: state.auth.isAdmin,
+        currentUser: user
     }
 }
 export default compose(connect(mapStateToProps), firestoreConnect([
     {
         collection: 'messages'
+    }, {
+        collection: 'users'
     }
 ]))(DashboardT);
